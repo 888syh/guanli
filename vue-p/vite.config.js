f97a51c57a9f7a6ab163cb79eff45af834404478
888syh <2752025982@qq.com>
2025-06-25 04:14:16 +0800

    fix: update vite config for production build

--- vue-p/vite.config.js
+++ vue-p/vite.config.js
@@ -6,13 +6,31 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [
     vue(),
     // vueDevTools(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    // 生成静态资源的存放路径
+    assetsInlineLimit: 4096, // 4kb
+    // 小于此阈值的导入或引用资源将内联为 base64 编码
+    sourcemap: false,
+    // 构建后是否生成 source map 文件
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vue-vendor': ['vue', 'vue-router'],
+          'element-plus-vendor': ['element-plus'],
+        },
+      },
     },
   },
 })
